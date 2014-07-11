1aced5409a6c4117655e9c88d2ecd7c8ec2783e9
Qurban Ali <qurban.ali@iceanimations.com>
2014-07-11 12:56:27 +0500

    uiContainer added

--- src/window.py
+++ src/window.py
@@ -1,10 +1,11 @@
 import site
 site.addsitedir(r"R:\Pipe_Repo\Users\Qurban\utilities")
-from uiContainer import uic
+try:
+    from uiContainer import uic
+except:
+    from PyQt4 import uic
 from PyQt4.QtGui import *
 from PyQt4.QtCore import Qt
-
-site.addsitedir(r"R:\Pipe_Repo\Users\Hussain\packages")
 import qtify_maya_window as qtfy
 
 import os.path as osp
